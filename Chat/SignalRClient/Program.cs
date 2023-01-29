18bbf4579bd8641fc83b3e48d11ea87d1401548f
Vkirovpz <Vkirovpz@students.softuni.bg>
2023-01-29 15:41:08 +0200

    SignalR Server and Client added

---
+++ Chat/SignalRClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+
+public class Program
+{
+    private static HubConnection _connection;
+    private static async Task Main(string[] args)
+    {
+        _connection = new HubConnectionBuilder()
+            .WithUrl("https://localhost:7021/chatHub")
+            .Build();
+
+        _connection.Closed += async (error) =>
+        {
+            await Task.Delay(new Random().Next(0, 5) * 1000);
+            await _connection.StartAsync();
+        };
+
+        await ConnectAsync();
+        Console.WriteLine("Enter your username");
+        var user = Console.ReadLine();
+
+        bool stop = false;
+        var message = Console.ReadLine();
+        while (!string.Equals(message, "qw!", StringComparison.CurrentCultureIgnoreCase))
+        {
+            Send(user, message);
+            message = Console.ReadLine();
+        }
+    }
+
+    private static async Task ConnectAsync()
+    {
+        _connection.On<string, string>("ReceiveMessage", (user, message) =>
+        {
+            Console.WriteLine($"{user}: {message}");
+        });
+        try
+        {
+            await _connection.StartAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: {0}", e);
+        }
+    }
+
+    private static async void Send(string user, string msg)
+    {
+        try
+        {
+            await _connection.InvokeAsync("SendMessage", user, msg);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Exception: {e}");
+        }
+    }
+}
+
